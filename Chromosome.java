2973e8af9ace950df19b4ca2b8cd8a9bf314ebfc
lithiumlkid <luvnenvy@gmail.com>
2018-04-12 16:45:28 +0800

    Reduce random gene generation range

--- Chromosome.java
+++ Chromosome.java
@@ -20,7 +20,7 @@ public class Chromosome {
     }
     public void generateIndividual() {
         for (int i = 0; i < size(); i++) {
-            double gene = random.nextDouble() * 1000.0f;
+            double gene = random.nextDouble() * 10.0f;
             genes[i] = gene;
         }
     }
